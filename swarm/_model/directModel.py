127754dca28da155030819eba318f15b87a7da6b
noec <noec@ethz.ch>
2022-10-04 20:41:27 +0200

    debug opt

--- swarm/_model/directModel.py
+++ swarm/_model/directModel.py
@@ -8,5 +8,5 @@ def evaluateModel(p):
     delrOrientation = p["Parameters"][1]
     delrAttraction = p["Parameters"][2]
     psi = p["Parameters"][3]
-    retvalue = objectivefunction(rRepulsion, delrOrientation, delrAttraction,psi)[1]
-    p["F(x)"] = retvalue
+    retvalue = objectivefunction(rRepulsion, delrOrientation, delrAttraction,psi)
+    p["F(x)"] = retvalue[1]
