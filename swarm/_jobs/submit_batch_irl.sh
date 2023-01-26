27aed64a2056e2e0c7dea80bb1699628707ef0d9
wadaniel <wadaniel@ethz.ch>
2023-01-26 23:31:44 +0100

    jobs

--- swarm/_jobs/submit_batch_irl.sh
+++ swarm/_jobs/submit_batch_irl.sh
@@ -1,21 +1,27 @@
-run=10000
+run=200
 
 export POL="Linear"
 #export POL="Quadratic"
 export EXP=5000000
 
-for EU in 2000 5000
+for EU in 5000 10000 25000
 do
-    for D in 1 2 4
+    for R in 8 32
+    do
+
+    for D in 1 4 8
     do 
         for B in 1 4 16 64
         do 
             run=$(($run+1))
             export RUN=$run
+            export RNN=$R
             export DBS=$D
             export BBS=$B
             export EBRU=$EU
             bsub < bsub-vracer-irl.lsf
         done
+    done
+
     done
 done
