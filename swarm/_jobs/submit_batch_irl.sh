7bfe0d682ec8f17a41f7f701c03a18227d536a91
wadaniel <wadaniel@ethz.ch>
2023-02-09 14:53:52 +0100

    tmp

--- swarm/_jobs/submit_batch_irl.sh
+++ swarm/_jobs/submit_batch_irl.sh
@@ -1,16 +1,20 @@
-run=500
+run=501
 
 export POL="Linear"
 #export POL="Quadratic"
 export EXP=2000000
 
-export N=25
-export NN=9
+export DIM=3
+export N=10
+export NN=3
+#export DIM=2
+#export N=25
+#export NN=9
 export NT=1000
 
-for EU in 5000 15000
+for EU in 5000 #15000
 do
-    for R in 8 32
+    for R in 8 #32
     do
 
     for D in 1 4 8
@@ -24,7 +28,6 @@ do
             export BBS=$B
             export EBRU=$EU
             bsub < bsub-vracer-irl.lsf
-            exit
         done
     done
 
--- swarm/_model/fish.py
+++ swarm/_model/fish.py
@@ -111,7 +111,7 @@ class fish:
         if np.linalg.norm(newWishedDirection) < 1e-12:
           newWishedDirection = self.curDirection
         
-        newWishedDirection /= np.linalg.norm(newWishedDirection)
+        #newWishedDirection /= np.linalg.norm(newWishedDirection)
         
         ## NOTE here the stocastic rotation of the direction of the fish gets calculated and applied
         # In the gautrais paper a rotational diffusion coefficent is introduced in order to make sure
@@ -121,11 +121,11 @@ class fish:
         # get random unit direction orthogonal to newWishedDirection
         # compute random angle from wrapped Gaussian ~ van Mises distribution
         if (self.normalDist):
-            randAngle = np.random.normal(0., self.sigma, 1, size=self.dim)
+            randAngle = np.random.normal(0., self.sigma, 1, size=self.dim-1)
         else:
-           randAngle = vonmises.rvs(1/self.sigma**2, size=self.dim)
-        #self.wishedDirection  = self.applyrotation(newWishedDirection, randAngle) #TODO: add again
-        self.wishedDirection = newWishedDirection #self.applyrotation(newWishedDirection, randAngle)
+           randAngle = vonmises.rvs(1/self.sigma**2, size=self.dim-1)
+        self.wishedDirection  = self.applyrotation(newWishedDirection, randAngle) #TODO: add again
+        #self.wishedDirection = newWishedDirection #self.applyrotation(newWishedDirection, randAngle)
         # print(len(self.wishedDirection)) this is 2
 
     def getAction(self):
@@ -186,8 +186,8 @@ class fish:
             elif dph < -np.pi:
                 dph += np.pi
 
-            action[0] = dth 
-            action[1] = dph
+            action[0] = dph 
+            action[1] = dth
 
         return action
 
@@ -270,7 +270,7 @@ class fish:
         
         elif(self.dim == 3):
 
-            assert np.isclose(np.linalg.norm(vectortoapply), 1.0), f"[fish] Vector {vectortoapply} not normalized {np.linalg.norm(vectortoapply)}"
+            
             r = np.linalg.norm(vectortoapply)
             th = np.arccos(vectortoapply[2]/r)
             phi = np.sign(vectortoapply[1])*np.arccos(vectortoapply[0]/np.linalg.norm(vectortoapply[:2]))
@@ -282,6 +282,8 @@ class fish:
             y = r*np.sin(th)*np.sin(phi)
             z = r*np.cos(th)
             wisheddir = np.array([x,y,z])
+            wisheddir /= np.linalg.norm(wisheddir)
+
             assert np.isclose(np.linalg.norm(wisheddir), 1.0), f"[fish] Wished dir {wisheddir} not normalized {np.linalg.norm(wisheddir)}"
 
             return wisheddir
