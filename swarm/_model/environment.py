0619b6b77c27c8013b96ce94a4f9b3415c7822fb
Daniel Waelchli <wadaniel@ethz.ch>
2023-03-02 18:21:41 +0100

    env plotting

--- swarm/_model/environment.py
+++ swarm/_model/environment.py
@@ -1,7 +1,12 @@
 from swarm import *
 from pathlib import Path
+from plotter3D import plotSwarm3DEnv
+
+episodeId = 0
 
 def environment( args, s ):
+
+    global episodeId
 
     # set set parameters and initialize environment
     numIndividuals       = args.N
@@ -17,6 +22,8 @@ def environment( args, s ):
     sampleId = s["Sample Id"]
     storeGoodEpisode = s["Custom Settings"]["Store Good Episodes"]
 
+    centerHistory = []
+    avgDistHistory = []
     locationHistory = []
     directionHistory = []
 
@@ -25,7 +32,7 @@ def environment( args, s ):
     else:
         seeds = [1, 2, 3, 4, 5]
 
-    seed = seeds[sampleId % len(seeds)]
+    seed = seeds[episodeId % len(seeds)]
     #numVectorsInState = args.dim
     numVectorsInState = 5
    
@@ -53,6 +60,19 @@ def environment( args, s ):
         print("Initial configuration is terminal state...")
 
     while (step < numTimesteps) and (not done):
+
+        if storeGoodEpisode == "True":
+            locations = []
+            directions = []
+            for fish in sim.fishes:
+                locations.append(fish.location.copy())
+                directions.append(fish.curDirection.copy())
+
+            centerHistory.append(sim.computeCenter())
+            avgDistHistory.append(sim.computeAvgDistCenter(centerHistory[-1]))
+            locationHistory.append(locations.copy())
+            directionHistory.append(directions.copy())
+
         if args.visualize:
             Path("./_figures").mkdir(parents=True, exist_ok=True)
             # fixed camera
@@ -65,9 +85,6 @@ def environment( args, s ):
             else:
                 print(f"Visualizing step {step}")
                 plotSwarm2D( sim, step, followcenter, step, numTimesteps)
-
-        sim.angularMoments.append(sim.computeAngularMom())
-        sim.polarizations.append(sim.computePolarisation())
 
    	# Getting new action
         s.update()
@@ -83,11 +100,15 @@ def environment( args, s ):
                 sim.fishes[i].curDirection = sim.fishes[i].applyrotation(currentDir, phi)
                 # update positions
                 sim.fishes[i].updateLocation()
+
         else:
             for i in np.arange(sim.N):
                 
                 sim.fishes[i].curDirection = sim.fishes[i].applyrotation(sim.fishes[i].curDirection, actions[i])
                 sim.fishes[i].updateLocation()
+
+        sim.angularMoments.append(sim.computeAngularMom())
+        sim.polarizations.append(sim.computePolarisation())
 
         # compute pair-wise distances and view-angles
         done = sim.preComputeStates()
@@ -105,22 +126,16 @@ def environment( args, s ):
         step += 1
         cumReward += rewards[0]
 
-        if storeGoodEpisode == "True":
-            locations = []
-            directions = []
-            for fish in sim.fishes:
-                locations.append(fish.location)
-                directions.append(fish.curDirection)
-
-            locationHistory.append(locations)
-            directionHistory.append(directions)
 
     if cumReward > 0.8:
-        fname = f"trajectory_{sampleId}.npz"
+        fname = f"trajectory_{episodeId}.npz"
         print(f"Dumping trajectory with cumulative reward {cumReward} to file {fname}")
-        locationHistory = np.array(locationHistory)
-        directionHistory = np.array(directionHistory)
-        np.savez(fname, cumReward=cumReward, locationHistory=locationHistory, directionHisory=directionHistory)
+        #print(f"locationHistory size {locationHistory.shape}")
+        #print(f"directionHistory size {directionHistory.shape}")
+        np.savez(fname, cumReward=cumReward, locationHistory=locationHistory, directionHisory=directionHistory, centerHistory=centerHistory, avgDistHistory=avgDistHistory)
+        plotSwarm3DEnv(episodeId, True, True, sim.N, locationHistory, directionHistory, centerHistory, avgDistHistory, sim.angularMoments, sim.polarizations)
+
+    episodeId += 1
 
     # Setting termination status
     if done:
