3d84f2c2c8322588a8a868ce2300313e0f6600b8
Daniel Waelchli <wadaniel@ethz.ch>
2023-08-08 12:49:51 +0200

    ffn training

--- swarm/_jobs/sbatch-ffn-daint.sh
+++ swarm/_jobs/sbatch-ffn-daint.sh
@@ -19,6 +19,6 @@ module purge
 module load daint-gpu gcc GSL cray-hdf5-parallel cray-python cdt-cuda craype-accel-nvidia60 VTK Boost/1.78.0-CrayGNU-21.09-python3
 
 cd ..
-#python3 run-tf-ffn.py --file _trajectories/observations_0o_25N_7NN_1000NT_50num_3d.json --NN 7 --obj 0 --epochs 500 --learningRate 0.00005
-#python3 run-tf-ffn.py --file _trajectories/observations_1o_25N_7NN_1000NT_50num_3d.json --NN 7 --obj 1 --epochs 500 --learningRate 0.00005
-python3 run-tf-ffn.py --file _trajectories/observations_2o_25N_7NN_1000NT_50num_3d.json --NN 7 --obj 2 --epochs 500 --learningRate 0.00005
+#python run-tf-ffn.py --file _trajectories/observations_0o_25N_7NN_1000NT_50num_3d.json --obj 0 --NN 7 --learningRate 0.0001 --steps 2000 --train
+#python run-tf-ffn.py --file _trajectories/observations_1o_25N_7NN_1000NT_50num_3d.json --obj 1 --NN 7 --learningRate 0.0001 --steps 2000 --train
+python run-tf-ffn.py --file _trajectories/observations_2o_25N_7NN_1000NT_50num_3d.json --obj 2 --NN 7 --learningRate 0.0001 --steps 2000 --train
--- swarm/run-tf-ffn.py
+++ swarm/run-tf-ffn.py
@@ -18,6 +18,7 @@ from tensorflow.keras import layers
 from tensorflow.keras import activations
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
+from tensorflow.keras.models import load_model
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -43,25 +44,37 @@ parser.add_argument(
     type=float,
     required=False)
 parser.add_argument(
-    '--eval',
+    '--train',
     action='store_true',
     required=False)    
 parser.add_argument(
-    '--file',
+    '--modelfile',
+    help='stored h5 file',
     type=str,
     required=False)    
-
-
-parser.add_argument('--file', help='trajectory file', required=True, type=str)
-args = parser.parse_args()
+parser.add_argument(
+    '--file',
+    help='trajectory file',
+    required=True,
+    type=str)
 
 print("Running FNN solver with arguments:")
+args = parser.parse_args()
 print(args)
 
-basedir = f"./shuffle_training_ffn_o{args.obj}/"
+basedir = f"./training_ffn_o{args.obj}/"
 filepath = basedir + 'cp-{epoch:04d}.ckpt'
 print(basedir)
-print(filepath)
+
+if args.train == False:
+    print(f"Loading model from {args.modelfile}")
+    model = load_model(args.modelfile)
+    model.summary()
+
+states = []
+actions = []
+testStates = []
+testActions = []
 
 # Opening JSON file
 f = open(args.file)
@@ -73,7 +86,7 @@ locations = np.array(data["Locations"])
 directions = np.array(data["Directions"])
 actionsRaw = np.array(data["Actions"])
 N, NT, NF, D = locations.shape
-#locations = locations.reshape((-1,D))
+
 print(locations.shape)
 print(directions.shape)
 _, _, _, NA = actionsRaw.shape
@@ -81,10 +94,6 @@ print(actionsRaw.shape)
 print(f"loaded {N} trajectories of length {NT} in {D}d, number of actions {NA}, swarm size {NF}")
 
 print("preprocessing data..")
-states = []
-actions = []
-testStates = []
-testActions = []
 for k in range(N):
     for t in range(NT): #NT
         sim = swarm( N=NF, numNN=args.NN,
@@ -94,21 +103,29 @@ for k in range(N):
         sim.preComputeStates()
         for fish in range(NF):
             if k < 3:
-                states.append(sim.getState(fish))
-                actions.append(actionsRaw[k,t,fish,:])
-            else:
                 testStates.append(sim.getState(fish))
                 testActions.append(actionsRaw[k,t,fish,:])
+            else:
+                states.append(sim.getState(fish))
+                actions.append(actionsRaw[k,t,fish,:])
 
 states = np.array(states)
 actions = np.array(actions)
-np.random.shuffle(states)
-np.random.shuffle(actions)
+testStates= np.array(testStates)
+testActions = np.array(testActions)
+
+#np.random.shuffle(states)
+#np.random.shuffle(actions)
 print("new states shape")
 print(states.shape)
-_, NS = states.shape
+#_, NS = states.shape
 print("new actions shape")
 print(actions.shape)
+
+print("test states shape")
+print(testStates.shape)
+print("test actions shape")
+print(testActions.shape)
 
 # Create a callback that saves the model's weights every 5 epochs
 cp_callback = tf.keras.callbacks.ModelCheckpoint(
@@ -125,20 +142,33 @@ v_checkpoint = tf.keras.callbacks.ModelCheckpoint(
     mode='auto'
 )
 
-model = Sequential()
-model.add(Dense(256, activation='tanh'))
-model.add(Dense(256,activation='tanh'))
-model.add(Dense(NA,activation=None))
-model.compile(loss='mse', optimizer='adam')
+if args.train == True:
 
-if args.eval == False:
+    model = Sequential()
+    model.add(Dense(128, activation='tanh'))
+    model.add(Dense(128,activation='tanh'))
+    model.add(Dense(NA,activation=None))
+    model.compile(loss='mse', optimizer='adam')
+
     opt = tf.keras.optimizers.Adam(learning_rate=args.learningRate)
-    history = model.fit(x=states, y=actions, num_epochs=None, steps=args.steps, batch_size=32, shuffle=True, validation_split=0.2, callbacks=[v_checkpoint])
+    history = model.fit(x=states, y=actions, epochs=args.steps, steps_per_epoch=1, batch_size=128, shuffle=True, validation_split=0.2, callbacks=[v_checkpoint])
     model.summary()
-    model.save(f'{basedir}final_model_o{args.obj}')
-else:
-    model.load_weights(args.file)
-    loss, acc = model.evaluate(testStates, testActions, verbose=2)
-    prediction = model.predict(testStates)
-    print(prediction)
-    print(prediction.shape)
+    model.save(f'{basedir}/final_o{args.obj}.h5')
+    np.save(f'{basedir}/history_o{args.obj}.npy',history.history)
+
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    print(history.history)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model mse')
+    plt.ylabel('loss')
+    plt.xlabel('steps')
+    plt.legend(['train', 'validation']) #, loc='upper left')
+    plt.savefig(f'{basedir}/history_mse_o{args.obj}.pdf')
+
+loss = model.evaluate(testStates, testActions, verbose=2)
+prediction = model.predict(testStates)
+print(prediction)
+print(prediction.shape)
